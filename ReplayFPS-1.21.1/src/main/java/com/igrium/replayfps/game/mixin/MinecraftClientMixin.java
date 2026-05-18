3fb236bc2216de6aa4a965dbb148f977c70081a7
Sigma47 <chinpatakdamdampro@gmail.com>
2026-05-18 12:53:41 +0530

    Update MinecraftClientMixin.java

--- ReplayFPS-1.21.1/src/main/java/com/igrium/replayfps/game/mixin/MinecraftClientMixin.java
+++ ReplayFPS-1.21.1/src/main/java/com/igrium/replayfps/game/mixin/MinecraftClientMixin.java
@@ -8,14 +8,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.igrium.replayfps.game.event.ClientJoinedWorldEvent;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.WorldLoadingState;
+import net.minecraft.client.gui.screen.DownloadingTerrainScreen;
 import net.minecraft.client.world.ClientWorld;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
 
     @Inject(method = "joinWorld", at = @At("TAIL"))
-    void replayfps$onJoinWorld(ClientWorld world, WorldLoadingState worldLoadingState, CallbackInfo ci) {
+    void replayfps$onJoinWorld(ClientWorld world, DownloadingTerrainScreen.WorldEntryReason reason, CallbackInfo ci) {
         ClientJoinedWorldEvent.EVENT.invoker().onJoinedWorld((MinecraftClient) (Object) this, world);
     }
 }
